09b77c9bd82c4a59a728d7ff30cdc0a2ea8a5ae8
danielgm <daniel.garcia@ezlo.com>
2024-03-28 10:15:56 -0500

    Change default picture

--- scripts/fehbg.nix
+++ scripts/fehbg.nix
@@ -4,7 +4,7 @@ pkgs.writeShellScriptBin "wallpaper-set" ''
   
   if [ -z "$wallpaper" ]; then
     # Default wallpaper if no argument is provided
-    wallpaper="/home/danielgm/.dots-flakes/wallpapers/f1_rain.jpg"
+    wallpaper="/home/danielgm/.dots-flakes/wallpapers/2017-red-bull.jpg"
   fi
 
   ${pkgs.pywal}/bin/wal -i "$wallpaper"
