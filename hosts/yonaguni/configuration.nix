8bda3f918953f95929e6156cdd53b8b2f87fd425
danielgm <daniel.garcia@ezlo.com>
2024-06-18 18:43:41 -0500

    Testing podman images as systemd

--- hosts/yonaguni/configuration.nix
+++ hosts/yonaguni/configuration.nix
@@ -101,6 +101,14 @@
     podman.dockerCompat = true;
     podman.defaultNetwork.settings.dns_enabled = true;
   };
+  virtualisation.oci-containers.backend = "podman";
+  virtualisation.oci-containers.containers = {
+    nginx-test = {
+      image = "docker.io/library/nginx:latest";
+      autoStart = true;
+      ports = [ "127.0.0.1:8080:80" ];
+    };
+  };
 
   # Fonst
   fonts = {
