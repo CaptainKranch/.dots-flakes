f0b296e2feb122577af6e47999a15ccb703c36e8
danielgm <daniel.garcia@ezlo.com>
2024-06-18 19:33:01 -0500

    Testing podman images as sysmed on host module

--- services/default.nix
+++ services/default.nix
@@ -13,11 +13,28 @@
         autoStart = true;
         ports = [ "0.0.0.0:8080:80" ]; # Make available to the network
       };
-      airflow = {
-        image = "docker.io/library/jellyfin:latest";
+      jellyfin = {
+        image = "docker.io/jellyfin/jellyfin:latest";
         autoStart = true;
-        ports = [ "0.0.0.0:8096:8096" ]; # Expose Airflow web server
+        ports = [ "0.0.0.0:8096:8096" ]; # Expose Jellyfin port
+        labels = {
+          "io.containers.autoupdate" = "registry";
+        };
+        devices = [
+          "/dev/dri:/dev/dri"
+        ];
+        environment = {
+          TZ = "America/Bogota";
+        };
+        volumes = [
+          "/home/danielgm/Documents/Services/jellyfin/cache:/cache:Z"
+          "/home/danielgm/Documents/Services/jellyfin/config:/config:Z"
+          "/home/danielgm/Documents/Services/jellyfin/media:/media:Z"
+        ];
+        user = "${toString (getent passwd danielgm).uid}:${toString (getent group danielgm).gid}";
+        userns = "keep-id";
       };
     };
   };
 }
+
