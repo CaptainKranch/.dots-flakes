4445d9ee9704719d128261fe6a7b7fe451bb4a29
Daniel Garcia Medina <danielgm32410@gmail.com>
2024-12-20 16:23:24 -0500

    Fix: reverting changes for some reason is not taking the path...

--- hosts/neaYork/configuration.nix
+++ hosts/neaYork/configuration.nix
@@ -10,12 +10,11 @@
 
     # You can also split up your configuration and import pieces of it here:
     #./modules/nix-core.nix
-    ./modules/arm-darwin/nix-core.nix
-    ./modules/arm-darwin/system.nix
-    ./modules/arm-darwin/apps.nix
-    ./modules/arm-darwin/host-users.nix
-    ./modules/arm-darwin/nix-to-mac.nix
-
+    ./modules/nix-core.nix
+    ./modules/system.nix
+    ./modules/apps.nix
+    ./modules/host-users.nix
+    ./modules/nix-to-mac.nix
 
     # home manager
     home-manager.darwinModules.home-manager
@@ -26,7 +25,6 @@
       home-manager.users.dgm = import ./home;
     }
   ];
-  
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.05";
