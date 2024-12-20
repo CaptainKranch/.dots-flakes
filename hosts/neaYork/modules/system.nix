fe0eb1377058217037f6268b8ae7468feb16647d
Daniel Garcia Medina <danielgm32410@gmail.com>
2024-12-20 16:19:28 -0500

    Revert "Testing..."
    This reverts commit 5f9b5eebaee57ededee0e24d7d38e7147905cd43.

---
+++ hosts/neaYork/modules/system.nix
@@ -0,0 +1,113 @@
+{ pkgs, ... }:
+
+  ###################################################################################
+  #
+  #  macOS's System configuration
+  #
+  #  All the configuration options are documented here:
+  #    https://daiderd.com/nix-darwin/manual/index.html#sec-options
+  #  Incomplete list of macOS `defaults` commands :
+  #    https://github.com/yannbertrand/macos-defaults
+  #
+  ###################################################################################
+{
+  system = {
+    stateVersion = 5;
+    # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
+    activationScripts.postUserActivation.text = ''
+      # activateSettings -u will reload the settings from the database and apply them to the current session,
+      # so we do not need to logout and login again to make the changes take effect.
+      /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+    '';
+
+    defaults = {
+      # menuExtraClock.Show24Hour = true;  # show 24 hour clock
+      
+      # customize dock
+      dock = {
+        autohide = true;
+        show-recents = false;  # disable recent apps
+
+        # customize Hot Corners(触发角, 鼠标移动到屏幕角落时触发的动作)
+        wvous-tl-corner = 2;  # top-left - Mission Control
+        wvous-tr-corner = 13;  # top-right - Lock Screen
+        wvous-bl-corner = 3;  # bottom-left - Application Windows
+        wvous-br-corner = 4;  # bottom-right - Desktop
+      };
+
+      # customize finder
+      finder = {
+        _FXShowPosixPathInTitle = true;  # show full path in finder title
+        AppleShowAllExtensions = true;  # show all file extensions
+        FXEnableExtensionChangeWarning = false;  # disable warning when changing file extension
+        QuitMenuItem = true;  # enable quit menu item
+        ShowPathbar = true;  # show path bar
+        ShowStatusBar = true;  # show status bar
+      };
+
+      # customize settings that not supported by nix-darwin directly
+      # Incomplete list of macOS `defaults` commands :
+      #   https://github.com/yannbertrand/macos-defaults
+      NSGlobalDomain = {
+        # `defaults read NSGlobalDomain "xxx"`
+        "com.apple.swipescrolldirection" = true;  # enable natural scrolling(default to true)
+        "com.apple.sound.beep.feedback" = 0;  # disable beep sound when pressing volume up/down key
+        AppleKeyboardUIMode = 3;  # Mode 3 enables full keyboard control.
+
+        # If you press and hold certain keyboard keys when in a text area, the key’s character begins to repeat.
+        # This is very useful for vim users, they use `hjkl` to move cursor.
+        # sets how long it takes before it starts repeating.
+        InitialKeyRepeat = 15;  # normal minimum is 15 (225 ms), maximum is 120 (1800 ms)
+        # sets how fast it repeats once it starts. 
+        KeyRepeat = 3;  # normal minimum is 2 (30 ms), maximum is 120 (1800 ms)
+
+      };
+    };
+
+    # keyboard settings is not very useful on macOS
+    # the most important thing is to remap option key to alt key globally,
+    # but it's not supported by macOS yet.
+    keyboard = {
+#      enableKeyMapping = true;  # enable key mapping so that we can use `option` as `control`
+#
+#      # NOTE: do NOT support remap capslock to both control and escape at the same time
+#      remapCapsLockToControl = false;  # remap caps lock to control, useful for emac users
+#      remapCapsLockToEscape  = true;   # remap caps lock to escape, useful for vim users
+#
+#      # swap left command and left alt 
+#      # so it matches common keyboard layout: `ctrl | command | alt`
+#      #
+#      # disabled, caused only problems!
+#      swapLeftCommandAndLeftAlt = false;  
+    };
+  };
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  # this is required if you want to use darwin's default shell - zsh
+  programs.zsh.enable = true;
+  environment.shells = [
+    pkgs.zsh
+  ];
+
+#  # Fonts
+#  fonts = {
+#    packages = with pkgs; [
+#      # icon fonts
+#      material-design-icons
+#      font-awesome
+#
+#      # nerdfonts
+#      # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/data/fonts/nerdfonts/shas.nix
+#      (nerdfonts.override {
+#        fonts = [
+#          # symbols icon only
+#          "NerdFontsSymbolsOnly"
+#          # Characters
+#          "FiraCode"
+#          "JetBrainsMono"
+#          "Iosevka"
+#        ];
+#      })
+#    ];
+#  };
+}
