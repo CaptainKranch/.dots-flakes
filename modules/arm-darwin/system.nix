bee2fb8cd448879927f1716d7b6f760888af5f5d
Daniel Garcia M <danielgm32410@gmail.com>
2025-01-21 21:11:10 -0500

    FIX: Removed `nushell` as program since it doens't exists...

--- modules/arm-darwin/system.nix
+++ modules/arm-darwin/system.nix
@@ -79,7 +79,6 @@
   # Create /etc/zshrc that loads the nix-darwin environment.
   # this is required if you want to use darwin's default shell - zsh
   programs.zsh.enable = true;
-  programs.nushell.enable = true;
   environment.shells = [
     pkgs.zsh
     pkgs.nushell
